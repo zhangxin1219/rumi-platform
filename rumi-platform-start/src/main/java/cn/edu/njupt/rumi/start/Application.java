2fab19449dcd56d8e5103cb55dff567737544316
zhangxin1219 <1290007051@qq.com>
2020-09-19 11:25:45 +0800

    创建start模块

---
+++ rumi-platform-start/src/main/java/cn/edu/njupt/rumi/start/Application.java
@@ -0,0 +1,19 @@
+package cn.edu.njupt.rumi.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author ZhangXin
+ * @date 2020/9/19 10:48
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
